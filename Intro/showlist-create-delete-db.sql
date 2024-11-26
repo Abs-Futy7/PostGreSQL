bca0fca998b423e6111f5c1d01d7ae741acb0f46
MD. Abu Bakar Siddique <140780598+Abs-Futy7@users.noreply.github.com>
2024-11-27 00:48:39 +0600

    Add files via upload

---
+++ Intro/showlist-create-delete-db.sql
@@ -0,0 +1,8 @@
+SELECT datname FROM pg_database;   -- show existing database
+
+CREATE DATABASE test;  -- create new database
+
+-------- switch to new db query: --------
+-- put mouse on that db and select query --
+
+DROP DATABASE test1;  -- to delete db test1
